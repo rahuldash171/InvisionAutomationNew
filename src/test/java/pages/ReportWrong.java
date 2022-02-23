e263c4c3eed81373e5e57df030204b1723fd9b00
rahuldash171 <rahul.dash@bcone.com>
2022-02-23 20:41:35 +0530

    HD change rahul

--- src/test/java/pages/ReportWrong.java
+++ src/test/java/pages/ReportWrong.java
@@ -1,3 +1,28 @@
-package pages;public class ReportWrong {
-@author - Rahul Dash
+package pages;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class ReportWrong {
+    /*@author -
+    Rahul Dash*/
+    public void ClickNBack(WebDriverWait wait, AndroidDriver driver, String packageName) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
+                packageName + ":id/report_damage_img"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
+                packageName + ":id/report_wrong"))).click();
+        String reportWrongLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.TextView"))).getText();
+        System.out.println("Report Wrong header : "+reportWrongLabel);
+        Assert.assertTrue(reportWrongLabel.equals("Report Wrong"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
+                packageName + ":id/action_home"))).click();
+        String hdLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
+                packageName + ":id/home_delivery_persona_label"))).getText();
+        //TODO - actually get this test to work
+        Assert.assertTrue(hdLabel.equals("Home Delivery"));
+    }
 }
